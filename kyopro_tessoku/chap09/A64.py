1fff699ca591797d2b8b30230ede6c22d2e0cbce
shining-ai <tderkkwii+social@gmail.com>
2023-12-26 21:11:11 +0000

    A64をリファクタリング

--- kyopro_tessoku/chap09/A64.py
+++ kyopro_tessoku/chap09/A64.py
@@ -5,14 +5,38 @@ import sys
 
 def debug_input():
     _INPUT = """\
-6 7
-1 2 15
-1 4 20
-2 3 65
-2 5 4
-3 6 50
-4 5 30
-5 6 8
+15 30
+10 11 23
+11 13 24
+5 8 22
+10 15 18
+12 14 15
+2 10 11
+4 7 15
+5 7 15
+7 9 8
+8 12 1
+5 14 1
+10 14 17
+10 12 11
+8 10 6
+7 14 28
+6 9 1
+1 10 19
+4 5 4
+9 10 21
+7 10 21
+4 10 29
+5 10 8
+4 14 8
+11 12 24
+10 13 13
+3 10 1
+5 12 24
+2 15 23
+6 10 18
+6 15 25
+
     """
     sys.stdin = io.StringIO(_INPUT)
 
@@ -22,41 +46,30 @@ def main():
     N, M = map(int, input().split())
     edges = [list(map(int, input().split())) for i in range(M)]
 
-    G = [list() for i in range(N + 1)]
+    net = [[] for _ in range(N + 1)]
     for a, b, c in edges:
-        G[a].append((b, c))
-        G[b].append((a, c))
+        net[a].append((b, c))
+        net[b].append((a, c))
 
-    INF = 10**10
-    kakutei = [False] * (N + 1)
-    cur = [INF] * (N + 1)
+    cur = [1 << 61] * (N + 1)
     cur[1] = 0
-    Q = []
-    heapq.heappush(Q, (cur[1], 1))
+    q = [(0, 1)]
+    heapq.heapify(q)
 
-    # ダイクストラ法
-    while len(Q) >= 1:
-        # 次に確定させるべき頂点を求める
-        # （ここでは、優先度付きキュー Q の最小要素を取り除き、その要素の 2 番目の値（頂点番号）を pos に代入する）
-        pos = heapq.heappop(Q)[1]
-
-        # Q の最小要素が「既に確定した頂点」の場合
-        if kakutei[pos] == True:
+    while q:
+        cost, pos = heapq.heappop(q)
+        if cur[pos] < cost:
             continue
-
-        # cur[x] の値を更新する
-        kakutei[pos] = True
-        for e in G[pos]:
-            # 書籍内のコードとは pos = e[0], cost = e[1] で対応している
-            if cur[e[0]] > cur[pos] + e[1]:
-                cur[e[0]] = cur[pos] + e[1]
-                heapq.heappush(Q, (cur[e[0]], e[0]))
+        for next_pos, next_cost in net[pos]:
+            if cur[next_pos] > cost + next_cost:
+                cur[next_pos] = cost + next_cost
+                heapq.heappush(q, ((cur[next_pos], next_pos)))
 
     for i in range(1, N + 1):
-        if cur[i] != INF:
-            print(cur[i])
+        if cur[i] == 1 << 61:
+            print(-1)
         else:
-            print("-1")
+            print(cur[i])
 
 
 if __name__ == "__main__":
