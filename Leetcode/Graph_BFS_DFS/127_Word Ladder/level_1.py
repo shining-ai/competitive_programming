3db9a89c72fcde988b4ac5f9c7306faf3f101d2c
shining-ai <tderkkwii+social@gmail.com>
2024-01-14 00:46:09 +0000

    両側から探索する方法を追加

--- Leetcode/Graph_BFS_DFS/127_Word Ladder/level_1.py
+++ Leetcode/Graph_BFS_DFS/127_Word Ladder/level_1.py
@@ -67,9 +67,62 @@ def solve_2(beginWord, endWord, wordList):
     return 0
 
 
+from collections import defaultdict
+
+
+def solve_3(beginWord, endWord, wordList):
+    length = 0
+    all_combo_dict = defaultdict(list)
+
+    def visitWordNode(queue, visited, others_visited):
+        queue_size = len(queue)
+        for _ in range(queue_size):
+            current_word = queue.popleft()
+            for i in range(length):
+                intermediate_word = (
+                    current_word[:i] + "*" + current_word[i + 1 :]
+                )
+
+                for word in all_combo_dict[intermediate_word]:
+                    if word in others_visited:
+                        return visited[current_word] + others_visited[word]
+                    if word not in visited:
+                        visited[word] = visited[current_word] + 1
+                        queue.append(word)
+
+        return None
+
+    if endWord not in wordList:
+        return 0
+
+    length = len(beginWord)
+
+    for word in wordList:
+        for i in range(length):
+            all_combo_dict[word[:i] + "*" + word[i + 1 :]].append(word)
+
+    queue_begin = collections.deque([beginWord])
+    queue_end = collections.deque([endWord])
+
+    visited_begin = {beginWord: 1}
+    visited_end = {endWord: 1}
+    ans = None
+
+    while queue_begin and queue_end:
+        if len(queue_begin) <= len(queue_end):
+            ans = visitWordNode(queue_begin, visited_begin, visited_end)
+        else:
+            ans = visitWordNode(queue_end, visited_end, visited_begin)
+        if ans:
+            return ans
+
+    return 0
+
+
 if __name__ == "__main__":
     strs = ["hot", "dot", "dog", "lot", "log", "cog"]
     beginWord = "hit"
     endWord = "cog"
     print(solve_1(beginWord, endWord, strs))
     print(solve_2(beginWord, endWord, strs))
+    print(solve_3(beginWord, endWord, strs))
