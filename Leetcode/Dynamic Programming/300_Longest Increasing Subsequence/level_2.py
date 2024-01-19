f254e1f9ade5279133704fff35424a83b4dfcc2e
shining-ai <tderkkwii+social@gmail.com>
2024-01-19 19:59:17 +0000

    リファクタリング

--- Leetcode/Dynamic Programming/300_Longest Increasing Subsequence/level_2.py
+++ Leetcode/Dynamic Programming/300_Longest Increasing Subsequence/level_2.py
@@ -1,14 +1,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = [nums[0]]
+        sub = [-float("inf")]
+        max_combo = 0
 
         for i_num in nums:
             combo = bisect.bisect_left(sub, i_num)
 
-            if combo >= len(sub):
+            if combo > max_combo:
                 sub.append(i_num)
+                max_combo = combo
 
             if sub[combo] > i_num:
                 sub[combo] = i_num
 
-        return len(sub)
+        return max_combo
