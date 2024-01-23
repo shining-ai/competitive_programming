0bba8b3ffbf6551ec6178195c57e0c058c787615
shining-ai <tderkkwii+social@gmail.com>
2024-01-23 13:38:33 +0000

    198を追加

--- Leetcode/Dynamic Programming/198_House Robber/level_1.py
+++ Leetcode/Dynamic Programming/198_House Robber/level_1.py
@@ -1,28 +1,43 @@
-import copy
-
-
-# O(n^2) time
+# O(n) time
 # O(n) space
 def solve_1(nums):
     n = len(nums)
+
     if n == 1:
         return nums[0]
-    money_dp = [[False] * (n * 400) for _ in range(n + 1)]
-    money_dp[0][0] = True
-    money_dp[1][0] = True
-    money_dp[1][nums[0]] = True
+
+    max_robbed_value = [0] * (n + 1)
+    max_robbed_value[1] = nums[0]
 
     for i, i_money in enumerate(nums[1:], start=2):
-        money_dp[i] = copy.deepcopy(money_dp[i - 1])
-        for j in range(n * 400):
-            if money_dp[i - 2][j]:
-                money_dp[i][j + i_money] = True
+        max_robbed_value[i] = max(
+            max_robbed_value[i - 1], max_robbed_value[i - 2] + i_money
+        )
 
-    for i in range(n * 400 - 1, -1, -1):
-        if money_dp[-1][i]:
-            return i
+    return max_robbed_value[-1]
+
+
+# O(n) time
+# O(1) space
+def solve_2(nums):
+    n = len(nums)
+
+    if n == 1:
+        return nums[0]
+
+    max_robbed_prev = nums[0]
+    max_robbed_prev_prev = 0
+    max_robbed_curr = 0
+
+    for i_money in nums[1:]:
+        max_robbed_curr = max(max_robbed_prev, max_robbed_prev_prev + i_money)
+        max_robbed_prev_prev = max_robbed_prev
+        max_robbed_prev = max_robbed_curr
+
+    return max_robbed_curr
 
 
 if __name__ == "__main__":
     nums = [1, 3, 1, 3, 100]
     print(solve_1(nums))
+    print(solve_2(nums))
