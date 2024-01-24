a6774ea6f7ec63ba299c61676a610e8823936505
shining-ai <tderkkwii+social@gmail.com>
2024-01-24 20:24:48 +0000

    読みやすく修正

--- Leetcode/Dynamic Programming/139_Word Break/level_2.py
+++ Leetcode/Dynamic Programming/139_Word Break/level_2.py
@@ -1,16 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp_str = [False] * (len(s) + 1)
-        dp_str[0] = True
+        dp_s = [False] * (len(s) + 1)
+        dp_s[0] = True
 
         for i in range(len(s) + 1):
             for j_word in wordDict:
-                if i < len(j_word):
+                word_length = len(j_word)
+
+                if i < word_length:
                     continue
 
-                if dp_str[i - len(j_word)]:
-                    if j_word == s[i - len(j_word) : i]:
-                        dp_str[i] = True
+                if s[i - word_length : i] == j_word:
+                    if dp_s[i - word_length]:
+                        dp_s[i] = True
                         break
 
-        return dp_str[-1]
+        return dp_s[-1]
